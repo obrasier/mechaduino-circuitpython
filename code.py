489e5384601c56434c3de92b5e947baf745328b5
Owen Brasier <owen.brasier@sydney.edu.au>
2020-02-14 13:34:56 +1100

    made Mechaduino object

--- code.py
+++ code.py
@@ -26,81 +26,13 @@ def cycle(p):
     while p:
         yield from p
 
-pin_values = ((True, False, False, True),
-              (True, True, False, False),
-              (False, True, True, False),
-              (False, False, True, True))
-pin_values_r = ((True, False, False, True),
-                (False, False, True, True),
-                (False, True, True, False),
-                (True, True, False, False))
-cycle_pin_values = cycle(pin_values)
-cycle_pin_values_r = cycle(pin_values_r)
-
-steps_per_rev = 200
-deg_per_step = 360 / steps_per_rev
-
-led = DigitalInOut(board.D13)
-led.direction = Direction.OUTPUT
-led.value = True
-
-spi = SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-spi.try_lock()
-spi.configure(baudrate=10000000, polarity=0, phase=1)
-spi.unlock()
-CS_PIN = board.A2
-cs = DigitalInOut(CS_PIN)
-cs.direction = Direction.OUTPUT
-
 def lerp(x, in_min=0, in_max=1023, out_min=0, out_max=5):
     '''linear interpolation. Same as arduino map.'''
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def read_encoder():
-    '''Send SPI signal to read the raw value from the magnetic encoder.'''
-    cs.value = False
-    buf = bytearray(2)
-    spi.try_lock()
-    spi.write_readinto(b'\xFF\xFF', buf)
-    reading = ((buf[0] << 8) | buf[0]) & 0B0011111111111111
-    cs.value = True
-    spi.unlock()
-    return reading
-
 def guess_angle(encoder):
     '''interpolate the raw encoder value to an angle'''
     return lerp(encoder, in_max=16383, out_max=360)
-
-def one_step(forwards=True):
-    if forwards:
-        in1.value, in3.value, in2.value, in4.value = next(cycle_pin_values)
-    else:
-        in1.value, in3.value, in2.value, in4.value = next(cycle_pin_values_r)
-
-def micro_one_step(theta):
-    phase_multiplier = steps_per_rev / 4
-    max_duty_cycle = 30000
-
-    angle = (phase_multiplier * theta) % 360
-    sin_a = math.cos(math.pi*angle/180)
-    sin_b = math.sin(math.pi*angle/180)
-    a_coil = int(sin_a * max_duty_cycle)
-    b_coil = int(sin_b * max_duty_cycle)
-    vr12.duty_cycle = abs(a_coil)
-    vr34.duty_cycle = abs(b_coil)
-    if a_coil >= 0:
-        in1.value = False
-        in2.value = True
-    else:
-        in1.value = True
-        in2.value = False
-    if b_coil >= 0:
-        in3.value = False
-        in4.value = True
-    else:
-        in3.value = True
-        in4.value = False
 
 def get_distance(current_angle, target):
     '''return the distance between two points on a circle.
@@ -131,44 +63,123 @@ def get_direction(curr, target, distance):
         if curr > target:
           return False
         else:
-          return True        
+          return True
 
+class Mechaduino:
+    # for one step rotations
+    pin_values = ((True, False, False, True),
+              (True, True, False, False),
+              (False, True, True, False),
+              (False, False, True, True))
+    pin_values_r = ((True, False, False, True),
+                (False, False, True, True),
+                (False, True, True, False),
+                (True, True, False, False))
+    cycle_pin_values = cycle(pin_values)
+    cycle_pin_values_r = cycle(pin_values_r)
 
-def goto(target, step_size=1):
-    '''Moves the motor to a target angle via the shortest path.
+    # generally useful constants
+    STEPS_PER_REV = 200
+    DEG_PER_STEP = 360 / STEPS_PER_REV
 
-    :param target: the target angle in degrees
-    :param step_size: the step size in degrees
-    '''
-    current_angle = guess_angle(read_encoder())
-    distance = get_distance(current_angle, target)
-    clockwise = get_direction(current_angle, target, distance)
-    num_steps = int(distance/step_size)
-    for step in range(num_steps):
-        if clockwise:
-            micro_one_step(step/step_size)
+    def __init__(self):
+        # pins setup. 
+        self.in2 = DigitalInOut(board.D7)
+        self.in2.direction = Direction.OUTPUT
+        self.in4 = DigitalInOut(board.D6)
+        self.in4.direction = Direction.OUTPUT
+        self.in1 = DigitalInOut(board.D8)
+        self.in1.direction = Direction.OUTPUT
+        self.in3 = DigitalInOut(board.D5)
+        self.in3.direction = Direction.OUTPUT
+
+        self.vr12 = PWMOut(board.D9, frequency=200000, duty_cycle=65000)
+        self.vr34 = PWMOut(board.D4, frequency=200000, duty_cycle=65000)
+        # SPI setup
+        self.spi = SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        self.cs = DigitalInOut(board.A2)
+        self._setup_spi()
+
+        # led setup
+        self.led = DigitalInOut(board.D13)
+        self.led.direction = Direction.OUTPUT
+        self.led.value = True
+    
+    def _setup_spi(self):
+        self.spi.try_lock()
+        self.spi.configure(baudrate=10000000, polarity=0, phase=1)
+        self.spi.unlock()
+        self.cs.direction = Direction.OUTPUT
+        self.cs = True
+
+    def read_encoder(self):
+    '''Send SPI signal to read the raw value from the magnetic encoder.'''
+        self.cs.value = False
+        buf = bytearray(2)
+        self.spi.try_lock()
+        self.spi.write_readinto(b'\xFF\xFF', buf)
+        reading = ((buf[0] << 8) | buf[0]) & 0B0011111111111111
+        self.cs.value = True
+        self.spi.unlock()
+        return reading
+
+    def one_step(self, forwards=True):
+        if forwards:
+            self.in1.value, self.in3.value, self.in2.value, self.in4.value = next(cycle_pin_values)
         else:
-            micro_one_step(-step/step_size)
+            self.in1.value, self.in3.value, self.in2.value, self.in4.value = next(cycle_pin_values_r)
 
-# A is in1/in2
+    def micro_one_step(self, theta):
+        phase_multiplier = STEPS_PER_REV / 4
+        max_duty_cycle = 30000
 
-in2 = DigitalInOut(board.D7)
-in2.direction = Direction.OUTPUT
-in4 = DigitalInOut(board.D6)
-in4.direction = Direction.OUTPUT
-in1 = DigitalInOut(board.D8)
-in1.direction = Direction.OUTPUT
-in3 = DigitalInOut(board.D5)
-in3.direction = Direction.OUTPUT
+        angle = (phase_multiplier * theta) % 360
+        sin_a = math.cos(math.pi*angle/180)
+        sin_b = math.sin(math.pi*angle/180)
+        a_coil = int(sin_a * max_duty_cycle)
+        b_coil = int(sin_b * max_duty_cycle)
+        self.vr12.duty_cycle = abs(a_coil)
+        self.vr34.duty_cycle = abs(b_coil)
+        if a_coil >= 0:
+            self.in1.value = False
+            self.in2.value = True
+        else:
+            self.in1.value = True
+            self.in2.value = False
+        if b_coil >= 0:
+            self.in3.value = False
+            self.in4.value = True
+        else:
+            self.in3.value = True
+            self.in4.value = False
 
-vr12 = PWMOut(board.D9, frequency=200000, duty_cycle=65000)
-vr34 = PWMOut(board.D4, frequency=200000, duty_cycle=65000)
+        def goto(self, target, step_size=1):
+            '''Moves the motor to a target angle via the shortest path.
 
+            :param target: the target angle in degrees
+            :param step_size: the step size in degrees
+            '''
+            current_angle = guess_angle(read_encoder())
+            distance = get_distance(current_angle, target)
+            clockwise = get_direction(current_angle, target, distance)
+            num_steps = int(distance/step_size)
+            for step in range(num_steps):
+                if clockwise:
+                    self.micro_one_step(step/step_size)
+                else:
+                    self.micro_one_step(-step/step_size)
 
-for i in range(10000):
-    micro_one_step(i/2)
-    # one_step()
-    #time.sleep(0.001)
+mech = Mechaduino()
+while True:
+    mech.goto(0)
+    time.sleep(0.01)
+    mech.goto(20)
+    time.sleep(0.01)
+
+# for i in range(10000):
+#     micro_one_step(i/2)
+#     # one_step()
+#     #time.sleep(0.001)
 
 # x = 0
 # while True:
